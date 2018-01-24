9e98223a8c9ca90f69309706ed629cb38eae8206
xieliming2017 <xieliming2006@hotmail.com>
2018-01-23 23:44:47 -0500

    kele6-messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,10 +6,18 @@ class Kele
   include HTTParty
   include Roadmap
   base_uri 'https://www.bloc.io/api/v1'
+  attr_accessor :email
 
   def initialize(email, password)
-    @options = { body: { email: email, password: password } }
-    response = self.class.post("/sessions", @options)
+    @email = email
+    options = {
+      body: {
+        "email": email,
+        "password": password
+      }
+    }
+
+    response = self.class.post("/sessions", options)
     @auth_token = response["auth_token"]
   end
 
@@ -21,6 +29,28 @@ class Kele
   def get_mentor_availability(mentor_id)
     response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
     JSON.parse(response.body)
+  end
+
+  def get_messages(page=1)
+    if page.nil?
+      response = self.class.get("/message_threads", headers: {"authorization" => @auth_token})
+    else
+      response = self.class.get("/message_threads", headers: {"authorization" => @auth_token})
+    end
+    JSON.parse(response.body)
+  end
+
+  def create_message(recipient_id, subject, stripped_text )
+    options = {
+      body: {
+        "sender": "#{@email}",
+        "recipient_id": recipient_id,
+        "subject": subject,
+        "stripped-text": stripped_text
+      }
+    }
+    response = self.class.post("/messages", options)
+
   end
 
 
