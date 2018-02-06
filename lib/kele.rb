4dec63e26965f37f6aae8d16bd52eb3d14e4e1f5
xieliming2017 <xieliming2006@hotmail.com>
2018-02-06 14:18:17 -0500

    kele extra credit. Checkpoint updating completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -58,17 +58,35 @@ class Kele
 
   end
 
-  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    enrollment_id = self.get_me["current_enrollment"]["id"]
     options = {
+      headers: {"authorization" => @auth_token},
       body: {
         "checkpoint_id": checkpoint_id,
         "assignment_branch": assignment_branch,
         "assignment_commit_link": assignment_commit_link,
-        "comment": comment
+        "comment": comment,
         "enrollment_id": enrollment_id
       }
     }
 
     response = self.class.post("/checkpoint_submissions", options)
+  end
+
+  def update_checkpoint(checkpoint_submission_id, assignment_branch, assignment_commit_link, checkpoint_id, comment )
+    enrollment_id = self.get_me["current_enrollment"]["id"]
+    options = {
+      headers: {"authorization" => @auth_token},
+      body: {
+        "id": checkpoint_submission_id,
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      }
+
+      response = self.class.post("/checkpoint_submissions", options)
   end
 end
